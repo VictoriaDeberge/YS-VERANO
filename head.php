8533b2fd8638da7ad21bf5983bdc5df21f949951
Ys <107312818+YSREZENDE@users.noreply.github.com>
2022-12-05 18:57:04 -0300

    colocando php

---
+++ head.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--definição icone de favoritos-->
+
+   <!-- Torna a página responsivo -->
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+ <!-- Bootstrap CSS -->
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+   <!-- Carrega folhas de estilo do aplicativo -->
+   <link rel="stylesheet" href="/style.css" />
+ 
+   <!-- Folhas de estilos de cada página serão carregadas aqui -->
+   <link rel="stylesheet" href="" id="pageCSS">
+   <!-- 
+      Importa a biblioteca Font Awesome via CDN
+      Para saber mais:
+        • https://fontawesome.com/
+    -->
+    <script src="https://kit.fontawesome.com/6c3bbfdabc.js" crossorigin="anonymous"></script>
+    <title>YS VERANO</title>
+</head>
+<body>
