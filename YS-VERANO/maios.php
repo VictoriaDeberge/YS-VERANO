e34d31e425d13f2fc9b954ac63e866321e33e007
Victoria Deberge Beauty Salon <112016091+VictoriaDeberge@users.noreply.github.com>
2023-02-08 17:38:00 -0300

    Add files via upload

--- YS-VERANO/maios.php
+++ YS-VERANO/maios.php
@@ -1,57 +1,27 @@
 <?php
 require_once 'head.php';
-require_once 'menu.php',
+require_once 'menu.php';
 
 ?>
 
+
 <div class="feito text-center">
-   <H1> FEITO PRA VOCÊ <H1>
+    <H1>FEITO PRA VOCÊ</H1>
+
 </div>
 <!--categoria bnt-->
-<button type="button" class="bnt" ><i class="fa-solid fa-list" ></i>Categorias</button>
+<button type="button" class="btn" ><i class="fa-solid fa-list"></i>Categorias</button>
 
 <div class="vendidos text-center">
-<p>Mais vendidos</p><div>
+<p>Mais vendidos</p></div>
 <!--card maios>
 <div class="row">
-    <div class="card preto">
-        <img class="image" src=mulher-morena-sexy-em-roupa-de-banho-banho.jpg
-        h2>Conjuntos maios
-       </h2> 
-       <p>R$ 200,00</p>
-       <p>Em até 3x de R$66,66 sem juros</p>
-       <a href="#" class "#" class="btn Btn-dark">adicionar ao carrinho</a>
-</div>
-</div>class "card vermelho"
-<img class="image" src=maio.jpg
-<h2>Conjunto Peace Love.
-        </h2>
+      <div class="card preto">
+        <img class="image" src=mulheranimada-em-maiotocandoocsbelo.jpg
+        h2>Conjuntos maios 
+        </h2>        
         <p>R$ 200,00</p>
         <p>Em até 3x de R$66,66 sem juros</p>
         <a  href="#" class="btn btn-dark">Adicionar ao Carrinho</a>
       </div>
- 
-     <div class="card terceiro">
-        <img class="image" src=maio.preto.jpg
-         <div class="vendidos text-center">
-     
-<p>arpoador</p></div>
-
-<img class="image ver" src=maio.preto.jpg
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      
