1ff141a54aa3668cc291af6585c4f4c74c6271f2
Victoria Deberge Beauty Salon <112016091+VictoriaDeberge@users.noreply.github.com>
2023-02-10 18:00:30 -0300

    Add files via upload

---
+++ maios.php
@@ -0,0 +1,75 @@
+<?php
+require_once 'head.php';
+require_once 'menu.php';
+
+?>
+
+
+<div class="feito text-center">
+    <H1>FEITO PRA VOCÊ</H1>
+
+</div>
+<!--categoria bnt-->
+<button type="button" class="btn" ><i class="fa-solid fa-list"></i>Categorias</button>
+
+<div class="vendidos text-center">
+<p>Mais vendidos</p></div>
+
+<!--cads maios-->
+<div class="row">
+      <div class="card preto">
+        <img class="image" src=morena-comfigura-perfeita-posando-sobre-rosa-em-trajes-de-banho-na-moda-vista-de-tras.jpg
+        <h2>Conjunto Peace Love.
+        </h2>
+        <p>R$ 200,00</p>
+        <p>Em até 3x de R$66,66 sem juros</p>
+        <a  href="#" class="btn btn-dark">Adicionar ao Carrinho</a>
+      </div>
+
+      <div class="card vermelho">
+     
+        <img class="image ver" src=retrato-do-modeo-bonito-caucasiano-mulher-bronzeada-com-cabelos-longos-escuros-no-meio-listrado-posando-na-praia-de-verao-com-areia-branca.jpg
+        <h2>Conjunto Peace Love.
+        </h2>
+        <p>R$ 200,00</p>
+        <p>Em até 3x de R$66,66 sem juros</p>
+        <a  href="#" class="btn btn-dark">Adicionar ao Carrinho</a>
+      </div>
+
+      <div class="card terceiro">
+        <img class="image" src=mulher-de-cabelo-encaralado-esportiva-em-boa-forma-tem-corpo-fino-cobre-os-olhos-com-o-braco-vestido-com-body-tem-pele-lisa-e-saudavel-pernas-delgadas-demonstra-figura-perfeita-isolada-sobre-fundo-bege.jpg
+        <h2>Conjunto Peace Love.
+        </h2>
+        <p>R$ 200,00</p>
+        <p>Em até 3x de R$66,66 sem juros</p>
+        <a  href="#" class="btn btn-dark">Adicionar ao Carrinho</a>
+      </div>
+      </div>
+      <!---->
+     
+      <div class="vendidos text-center">
+     
+<p>arpoador</p></div>
+<!--cads maios--> 
+<div class="row" id="arpoador">
+      <div class="card preto">
+        <img class="image" src=
+        <h2>Conjunto Peace Love.
+        </h2>
+        <p>R$ 200,00</p>
+        <p>Em até 3x de R$66,66 sem juros</p>
+        <a  href="#" class="btn btn-dark">Adicionar ao Carrinho</a>
+      </div>
+  <!--fechamento #arpoador-->
+      <div class="card vermelho">
+     
+        <img class="image ver" src=
+        <h2>Conjunto Peace Love.
+        </h2>
+        <p>R$ 200,00</p>
+        <p>Em até 3x de R$66,66 sem juros</p>
+        <a  href="#" class="btn btn-dark">Adicionar ao Carrinho</a>
+      </div>
+ 
+     <div class="card terceiro">
+        <img class="image" src=
