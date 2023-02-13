7ae54ddeb059f9306e588a35859bb328e6664c6e
Victoria Deberge Beauty Salon <112016091+VictoriaDeberge@users.noreply.github.com>
2023-02-13 09:56:44 -0300

    Add files via upload

--- YS-VERANO/maios.php
+++ YS-VERANO/maios.php
@@ -14,14 +14,134 @@ require_once 'menu.php';
 
 <div class="vendidos text-center">
 <p>Mais vendidos</p></div>
-<!--card maios>
+
+<!--cads maios-->
 <div class="row">
       <div class="card preto">
-        <img class="image" src=mulheranimada-em-maiotocandoocsbelo.jpg
-        h2>Conjuntos maios 
-        </h2>        
+        <img class="image" src=morena-comfigura-perfeita-posando-sobre-rosa-em-trajes-de-banho-na-moda-vista-de-tras.jpg
+        h2>Conjunto Peace Love.
+        </h2>
         <p>R$ 200,00</p>
         <p>Em até 3x de R$66,66 sem juros</p>
         <a  href="#" class="btn btn-dark">Adicionar ao Carrinho</a>
       </div>
-      
+
+      <div class="card vermelho">
+     
+        <img class="image ver" src=retrato-do-modeo-bonito-caucasiano-mulher-bronzeada-com-cabelos-longos-escuros-no-meio-listrado-posando-na-praia-de-verao-com-areia-branca.jpg
+        h2>Conjunto Peace Love.
+        </h2>
+        <p>R$ 200,00</p>
+        <p>Em até 3x de R$66,66 sem juros</p>
+        <a  href="#" class="btn btn-dark">Adicionar ao Carrinho</a>
+      </div>
+
+      <div class="card terceiro">
+        <img class="image" src=mulher-de-cabelo-encaralado-esportiva-em-boa-forma-tem-corpo-fino-cobre-os-olhos-com-o-braco-vestido-com-body-tem-pele-lisa-e-saudavel-pernas-delgadas-demonstra-figura-perfeita-isolada-sobre-fundo-bege.jpg
+       h2>Conjunto Peace Love.
+        </h2>
+        <p>R$ 200,00</p>
+        <p>Em até 3x de R$66,66 sem juros</p>
+        <a  href="#" class="btn btn-dark">Adicionar ao Carrinho</a>
+      </div>
+      </div>
+      <!---->
+     
+      <div class="vendidos text-center">
+     
+<p>arpoador</p></div>
+<!--cads maios--> 
+<div class="row" id="arpoador">
+      <div class="card preto">
+        <img class="image" src=mulher-jovem-e-bonita-sexy-hipster-sorridente-em-oculos-de-sol-garota-de-maio-de-ervilhas-de-verao-maio-posando-perto-da-parede-azul-bebendo-bebida-smoozy-cocktail-fresca-mostra-o-sinal-de-paz.jpg
+        h2>Conjunto Peace Love.
+        </h2>
+        <p>R$ 200,00</p>
+        <p>Em até 3x de R$66,66 sem juros</p>
+        <a  href="#" class="btn btn-dark">Adicionar ao Carrinho</a>
+      </div>
+  <!--fechamento #arpoador-->
+      <div class="card vermelho">
+     
+        <img class="image ver" src=retrato-do-modelo-bonito-caucasiano-mulher-bronzeada-com-cabelos-longos-escuros-no-maio-listrado-posando-na-praia-de-verao-areia-branca.jpg
+        h2>Conjunto Peace Love.
+        </h2>
+        <p>R$ 200,00</p>
+        <p>Em até 3x de R$66,66 sem juros</p>
+        <a  href="#" class="btn btn-dark">Adicionar ao Carrinho</a>
+      </div>
+ 
+     <div class="card terceiro">
+        <img class="image" src=mulher-jovem-e-bonita-de-cabelos-ruivos-feliz-vestida-com-maio-amarelo-mantendo-a-mao-erguida-atras-da-cabeca-e-sorrindo-amplamente-enquanto-olha-para-o-lado-isolada-sobre-fundo-branco.jpg
+        h2>Conjunto Peace Love.
+        </h2>
+        <p>R$ 200,00</p>
+        <p>Em até 3x de R$66,66 sem juros</p>
+        <a  href="#" class="btn btn-dark">Adicionar ao Carrinho</a>
+      </div>
+      </div>
+    
+   
+
+
+
+
+
+
+
+
+
+
+
+
+      <div class="vendidos text-center">
+<p> Maios </p></div>
+<!--cads maios-->
+<div class="row">
+      <div class="card preto">
+        <img class="image" src=        alt="money" />        
+        <h2>Conjunto Peace Love.
+        </h2>
+        <p>R$ 200,00</p>
+        <p>Em até 3x de R$66,66 sem juros</p>
+        <a  href="#" class="btn btn-dark">Adicionar ao Carrinho</a>
+      </div>
+ 
+      <div class="card vermelho">
+     
+        <img class="image ver" src="         alt="settings" />
+        <h2>Conjunto Peace Love.
+        </h2>
+        <p>R$ 200,00</p>
+        <p>Em até 3x de R$66,66 sem juros</p>
+        <a  href="#" class="btn btn-dark">Adicionar ao Carrinho</a>
+      </div>
+ 
+     <div class="card terceiro">
+        <img class="image" src="             alt="article" />
+        <h2>Conjunto Peace Love.
+        </h2>
+        <p>R$ 200,00</p>
+        <p>Em até 3x de R$66,66 sem juros</p>
+        <a  href="#" class="btn btn-dark">Adicionar ao Carrinho</a>
+      </div>
+      </div>
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+require_once 'footer.php'
+?>
